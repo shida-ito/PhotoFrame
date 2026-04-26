3f07c158381d841b5596c0d231ef5e094b08fa3f
shida-ito <shida.fio@gmail.com>
2026-04-27 01:16:12 +0900

    Bump app version to 1.1.2

--- build_app.sh
+++ build_app.sh
@@ -46,9 +46,9 @@ cat > "${CONTENTS}/Info.plist" << 'PLIST'
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.1.1</string>
+    <string>1.1.2</string>
     <key>CFBundleVersion</key>
-    <string>1.1.1</string>
+    <string>1.1.2</string>
     <key>LSMinimumSystemVersion</key>
     <string>14.0</string>
     <key>NSHighResolutionCapable</key>
